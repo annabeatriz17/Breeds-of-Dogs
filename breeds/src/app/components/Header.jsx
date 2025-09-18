2612d819c06f57aeafb6aa981aa56d7a946c81a6
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-09-17 21:41:02 -0300

    Refactor header styles and navigation links
    Moved Header styles to a new location and updated their design. Simplified Home and Perfil module CSS by removing header and navigation styles. Updated Header component to use new styles and revised navigation links. Changed global font family and renamed layout.js to layout.jsx.

--- breeds/src/app/components/Header.jsx
+++ breeds/src/app/components/Header.jsx
@@ -1,16 +1,19 @@
-import styles from "./Header.module.css";
+import styles from "../styles/Header.module.css";
 import Image from "next/image";
 
 export default function Header() {
     return (
-        <div className={styles.header}>
-            <div className={styles.left}>
-                <Image src="/icon/faviconn.ico" alt="Latido e Lambida" width={40} height={40} />
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <Image src="/icon/faviconn.ico" alt="Latido e Lambida" width={90} height={90} />
                 <h1 className={styles.title}>Latido & Lambida</h1>
             </div>
             <nav className={styles.nav}>
-                <a href="/Dogs" className={styles.navLink}>Cachorros</a>
-                <a href="/Apiinfo" className={styles.navLink}>Informações</a>
+                <a href="/Perfil" className={styles.navLink}>Perfil</a>
+                <a href="/Apiinfo" className={styles.navLink}>Api</a>
+                <a href="/Dogs" className={styles.navLink}>Listagem</a>
+                <a href="/Breeds" className={styles.navLink}>Detalhes</a>
+                <a href="/Contact" className={styles.navLink}>Contato</a>
             </nav>
         </div>
     );
