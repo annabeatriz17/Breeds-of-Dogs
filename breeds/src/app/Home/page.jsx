4e6078893f9e0bc8366bca33063c284b7a7b1bf7
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-09-23 11:54:27 -0300

    Revamp Home page layout and images
    Replaces dogs.png with individual dog images and adds new images (capa.png, osso.png, dog1-6.png). Updates Home.module.css for improved layout and styling, and refactors Home/page.jsx to use Next.js Image components, add new sections, and display individual dog images. Enhances visual presentation and content for the Home page.

--- breeds/src/app/Home/page.jsx
+++ breeds/src/app/Home/page.jsx
@@ -2,16 +2,43 @@
 import styles from "./Home.module.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Image from "next/image";
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <Header />
             <div className={styles.content}>
-                <h1 className={styles.title}>Latido & Lambida</h1>
-                <p className={styles.description}>Seja bem-vindo</p>
+                <div className={styles.sessao}>
+                    <div className={styles.osso}>
+                        <Image src="/images/osso.png" alt="Osso de cachorro" width={300} height={300} />
+                    </div>
+                    <h1 className={styles.title}>Latido & Lambida</h1>
+                    <p className={styles.description}>Pet shop completo com rações premium, banho e tosa, clínica veterinária, leva e traz. Uma linha premium para você e amor aos animais desde 2016!</p>
+                </div>
+                
+                <div className={styles.capa}>
+                    <Image src="/images/capa.png" alt="Dog" width={700} height={700} />
+                </div>
             </div>
+            <div className={styles.section}>
+                <p className={styles.subtitle}>VEJA AS FOFURAS</p>
+                <h2 className={styles.sectionTitle}>Que temos por aqui!</h2>
+                <p className={styles.sectionDescription}>
+                    Adoção de "Latido & Lambida. Aqui você encontra o carinho - ideal para quando você precisa viajar.
+                </p>
+                
+                <div className={styles.dogs}>
+                    <Image src="/images/dog1.png" alt="Dog 1" width={100} height={100} />
+                    <Image src="/images/dog2.png" alt="Dog 2" width={100} height={100} />
+                    <Image src="/images/dog3.png" alt="Dog 3" width={100} height={100} />
+                    <Image src="/images/dog4.png" alt="Dog 4" width={100} height={100} />
+                    <Image src="/images/dog5.png" alt="Dog 5" width={100} height={100} />
+                    <Image src="/images/dog6.png" alt="Dog 6" width={100} height={100} />
+                </div>
+            </div>
+            
             <Footer />
         </div>
     );
-}
+}   
