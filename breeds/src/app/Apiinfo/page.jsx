6fe0cec94cc4c5e221fd28f22c9e1dae9fddf538
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-10-07 11:55:39 -0300

    Refactor Apiinfo page layout and styles
    Simplified the Apiinfo page by removing detailed API information and replacing it with a header, title, and buttons linking to the API base URL and documentation. Updated CSS class names and structure for improved layout and consistency.

--- breeds/src/app/Apiinfo/page.jsx
+++ breeds/src/app/Apiinfo/page.jsx
@@ -1,46 +1,18 @@
 "use client";
-import styles from "./Apiinfo.module.css";
+import styles from "./apiinfo.module.css";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { Button } from "antd";
 
-export default function ApiInfo() {
+export default function Apiinfo() {
   return (
-    <div className={styles.apiContainer}>
-      <h1 className={styles.apiTitle}>
-        API escolhida: The Dog API
-      </h1>
-      
-      <p>
-        <strong>Documentação oficial:</strong>{" "}
-        <a href="https://thedogapi.com/" target="_blank" rel="noopener noreferrer" className={styles.apiLink}>
-          https://thedogapi.com/
-        </a>
-      </p>
-      <p>
-        <strong>URL base usada:</strong> <code className={styles.apiCode}>https://api.thedogapi.com/v1/</code>
-      </p>
-      <p>
-        <strong>Endpoint escolhido:</strong> <code className={styles.apiCode}>/breeds</code>
-      </p>
-      <p>
-        <strong>Atributos recebidos:</strong>
-        <ul className={styles.apiList}>
-          <li><code>id</code></li>
-          <li><code>name</code></li>
-          <li><code>breed_group</code></li>
-          <li><code>origin</code></li>
-          <li><code>bred_for</code></li>
-          <li><code>life_span</code></li>
-          <li><code>temperament</code></li>
-          <li><code>height</code></li>
-          <li><code>weight</code></li>
-          <li><code>reference_image_id</code></li>
-        </ul>
-      </p>
-      <p>
-        <strong>Descrição:</strong><br />
-        A <b>The Dog API</b> disponibiliza informações detalhadas sobre raças de cachorros, incluindo nome, grupo, origem, finalidade, expectativa de vida, temperamento, altura, peso e imagens. É ideal para projetos que precisam exibir ou pesquisar dados sobre cães.
-      </p>
-
+    <div className={styles.container}>
+      <Header />
+      <h1 className={styles.title}>The Dog API</h1>
+      <div className={styles.buttons}>
+        <Button type="primary" href="https://api.thedogapi.com/v1/breeds" target="_blank" rel="noopener noreferrer" style={styles.button2}>URL BASE</Button>
+        <Button type="primary" href="https://thedogapi.com/" target="_blank" rel="noopener noreferrer">API DOCUMENTATION</Button>
+      </div>
       <Footer />
     </div>
   );
